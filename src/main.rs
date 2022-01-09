c84d49932bbc1dfca9d1941a677ddebd783c6035
Xsuto <michal.sutowicz@gmail.com>
2022-01-09 01:07:37 +0100

    new args system added support for short and long arguments Now search_for feels more like find

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,20 @@ use std::path::{Path, PathBuf};
 use rayon::prelude::*;
 use regex;
 use regex::Regex;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    dir: String,
+    #[clap(short,long,default_value="*")]
+    name: String,
+    #[clap(short,long,default_value="")]
+    exclued_dirs: String,
+}
 
 #[derive(Debug)]
 struct FormattedArgs {
-    files: String,
     files_as_regex: Regex,
     searched_directory: PathBuf,
     excluded_dirs: Vec<Regex>,
@@ -19,39 +29,23 @@ fn format_path(path: String) -> String {
     path.replace("~", std::env::home_dir().unwrap().to_str().unwrap())
 }
 
-fn get_formatted_args(mut args: env::Args) -> FormattedArgs {
-    args.next();
-    let files = match args.next() {
-        None => {
-            eprintln!("Please provide file or wildcard that I will be looking for :P ");
-            process::exit(0);
-        }
-        Some(n) => n
-    };
-    let searched_directory = match args.next() {
-        None => PathBuf::from(format_path(String::from("."))),
-        Some(n) => PathBuf::from(format_path(n))
-    };
+fn get_formatted_args(args: Args) -> FormattedArgs {
+    let searched_directory =  PathBuf::from(format_path(args.dir));
     let mut excluded_dirs = vec![];
-    let excluded_dirs_as_string = match args.next() {
-        None => String::from(""),
-        Some(n) => n
-    };
 
-    for excluded_dir in excluded_dirs_as_string.split(",").by_ref().into_iter() {
+    for excluded_dir in args.exclued_dirs.split(",").by_ref().into_iter() {
         if &excluded_dir != &"" {
             excluded_dirs.push(Regex::new(excluded_dir).unwrap());
         }
     }
     FormattedArgs {
-        files_as_regex: get_files_name_as_regex(&files),
-        files,
+        files_as_regex: get_files_name_as_regex(&args.name),
         searched_directory,
         excluded_dirs,
     }
 }
 
-fn check_for_file(files: &String, files_as_regex: &Regex, searched_directory: &Path, excluded_dirs: &Vec<Regex>) {
+fn check_for_files(files_as_regex: &Regex, searched_directory: &Path, excluded_dirs: &Vec<Regex>) {
     if let Ok(entries) = fs::read_dir(searched_directory) {
         entries.par_bridge().for_each(|entry| {
             if let Ok(entry) = entry {
@@ -60,10 +54,10 @@ fn check_for_file(files: &String, files_as_regex: &Regex, searched_directory: &P
                     Err(_) => return
                 };
                 if metadata.is_dir() && (excluded_dirs.len() == 0 || !excluded_dirs.iter().any(|p| { p.is_match(entry.path().to_str().unwrap()) })) {
-                    check_for_file(&files, &files_as_regex, &entry.path(), &excluded_dirs);
+                    check_for_files(&files_as_regex, &entry.path(), &excluded_dirs);
                 }
                 let file_name = entry.file_name().into_string().unwrap();
-                if files_as_regex.is_match(&file_name) {
+                 if files_as_regex.is_match(&file_name) {
                     println!("{}", entry.path().display().to_string());
                 }
             }
@@ -75,7 +69,7 @@ fn get_files_name_as_regex(file_name: &String) -> Regex {
     let mut temp_regex = String::from("^(");
     for letter in file_name.chars() {
         if letter == '*' {
-            temp_regex.push_str("[A-Za-z0-9]*");
+            temp_regex.push_str(".*");
         } else if letter == '.' {
             temp_regex.push_str("\\.");
         }
@@ -98,7 +92,9 @@ fn setup() {
 
 fn main() {
     setup();
-    let args = get_formatted_args(env::args());
+    let args = Args::parse();
+    let args = get_formatted_args(args);
+    // let args = get_formatted_args(env::args());
     // println!("{:#?}", args);
-    check_for_file(&args.files, &args.files_as_regex, &args.searched_directory, &args.excluded_dirs);
+    check_for_files(&args.files_as_regex, &args.searched_directory, &args.excluded_dirs);
 }
